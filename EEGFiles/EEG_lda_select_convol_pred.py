5751e8d5d29d82ae7b7966e8e389e1b875670ce6
NiubilityDiu <mtianwen@umich.edu>
2020-01-06 10:07:27 -0500

    Inference and prediction for real data
    We save mcmc results for all channels, and extract/use subset channels for inference/prediction use.

---
+++ EEGFiles/EEG_lda_select_convol_pred.py
@@ -0,0 +1,159 @@
+import sys
+sys.path.insert(0, './self_py_fun')
+# from self_py_fun.MMAR_q_latent_z_multi import *
+import self_py_fun.EEGConvolGlobal as gc
+from self_py_fun.xDAFun import *
+print(os.getcwd())
+# tf.compat.v1.random.set_random_seed(612)
+# np.random.seed(612)
+
+u = 8
+alpha_s = 5.0
+beta_s = 5.0
+zeta_lambda = 1e-4 * np.ones([gc.num_electrode])
+zeta_s = 5e-3 * np.ones([gc.num_electrode])
+zeta_rho = 1e-4 * np.ones([gc.num_electrode])
+ki = 0.4
+scale_1 = 0.15
+scale_2 = 0.2
+std_bool = True
+beta_ising = 0.1
+gamma_neighbor = 2
+plot_threshold = 0.5
+a = 1  # weight for target
+b = 5  # weight for non-target
+sim_note = 'std_bool={}, kappa={}'.format(std_bool, gc.kappa)
+print(sim_note)
+
+trn_repetitions = [5]
+
+for _, trn_repetition in enumerate(trn_repetitions):
+
+    LDAGibbsObj = XDAGibbs(
+        # hyper-parameters:
+        sigma_sq_delta=100,
+        mu_1_delta=np.zeros([gc.num_electrode, u, 1]),  # Later on, we need to change the mu_1_delta by OLS (done!)
+        mu_0_delta=np.zeros([gc.num_electrode, u, 1]),
+        u=u, a=a, b=b, kappa=gc.kappa, letter_dim=gc.letter_dim, trn_repetition=trn_repetition,
+        # EEGPreFun
+        data_type=gc.data_type,
+        sub_folder_name=gc.sub_file_name,
+        # EEGGeneralFun
+        num_repetition=gc.num_repetition, num_electrode=gc.num_electrode,
+        flash_and_pause_length=gc.flash_and_pause_length,
+        num_letter=gc.letter_dim, n_multiple=gc.n_multiple,
+        local_bool=gc.local_use
+    )
+
+    phi_val, phi_fn = LDAGibbsObj.create_gaussian_kernel_fn(
+        scale_1=scale_1, u=u, ki=ki, scale_2=scale_2, display_plot=False
+    )
+    phi_val = np.tile(phi_val[:, np.newaxis], [1, gc.num_electrode])
+    phi_fn = np.tile(phi_fn[np.newaxis, ...], [gc.num_electrode, 1, 1])
+    # print('phi_val has shape {}'.format(phi_val.shape))
+    # print('phi_fn has shape {}'.format(phi_fn.shape))
+
+    phi_fn_inner_prod = np.transpose(phi_fn, [0, 2, 1]) @ phi_fn
+    phi_fn_inner_prod_inv = LDAGibbsObj.compute_hermittan_matrix_inv(phi_fn_inner_prod)
+    phi_fn_ols_operator = phi_fn_inner_prod_inv @ np.transpose(phi_fn, [0, 2, 1])
+    # print('phi_fn_ols_operator has shape {}'.format(phi_fn_ols_operator.shape))
+
+    # Import the training set without subsetting yet
+    [signals, eeg_code, eeg_type] = LDAGibbsObj.import_eeg_processed_dat(gc.file_subscript, reshape_to_1d=False)
+
+    eeg_code = np.reshape(eeg_code, [gc.letter_dim, gc.num_repetition * gc.num_rep])
+    eeg_type = np.reshape(eeg_type, [gc.letter_dim, gc.num_repetition * gc.num_rep])
+
+    print('trn_repetition = {}'.format(trn_repetition))
+
+    [signals_all,
+     train_x_mat_tar, train_x_mat_ntar,
+     train_tar_mean, train_ntar_mean,
+     train_x_tar_sum, train_x_ntar_sum,
+     train_x_tar_indices, train_x_ntar_indices,
+     eeg_code_3d] = LDAGibbsObj.obtain_pre_processed_signals(
+            signals, eeg_code, eeg_type, std_bool=std_bool
+    )
+
+    # Use signals_train_t_mean and signals_train_nt_mean to compute mu_1/mu_0
+    # train_t_mean = phi_fn @ mu_1_delta, train_nt_mean = phi_fn @ mu_0_delta
+    # LDAGibbsObj.mu_1_delta = phi_fn_ols_operator @ train_tar_mean
+    # LDAGibbsObj.mu_0_delta = phi_fn_ols_operator @ train_ntar_mean
+    # Or without any prior information
+    LDAGibbsObj.mu_1_delta = phi_fn_ols_operator @ np.zeros_like(train_tar_mean)
+    LDAGibbsObj.mu_0_delta = phi_fn_ols_operator @ np.zeros_like(train_ntar_mean)
+
+    # Compute preliminary s_sq_est
+    s_sq_est = np.var(np.concatenate([train_x_mat_tar, train_x_mat_ntar], axis=0), axis=(0, 2))
+    [delta_tar_mcmc, delta_ntar_mcmc,
+     lambda_mcmc, gamma_mcmc,
+     s_sq_mcmc, rho_mcmc,
+     log_lhd_mcmc] = LDAGibbsObj.import_bayes_lda_mcmc()
+
+    delta_tar_mcmc_mean = np.mean(delta_tar_mcmc, axis=0)
+    delta_ntar_mcmc_mean = np.mean(delta_ntar_mcmc, axis=0)
+    lambda_mcmc_mean = np.mean(lambda_mcmc, axis=0)
+    gamma_mcmc_mean = np.mean(gamma_mcmc, axis=0)
+    s_sq_mcmc_mean = np.mean(s_sq_mcmc, axis=0)
+    rho_mcmc_mean = np.mean(rho_mcmc, axis=0)
+
+    message_eeg = gc.sub_file_name
+    # Truncated mean with standardization
+    LDAGibbsObj.save_lda_selection_indicator(
+        train_tar_mean, train_ntar_mean,
+        lambda_mcmc_mean, gamma_mcmc_mean,
+        message_eeg, gc.sub_file_name[:4], phi_fn, threshold=plot_threshold, mcmc=False
+    )
+
+    beta_tar_mcmc = phi_fn[np.newaxis, ...] @ (lambda_mcmc[..., np.newaxis, np.newaxis] * delta_tar_mcmc)
+    beta_ntar_mcmc = phi_fn[np.newaxis, ...] @ (lambda_mcmc[..., np.newaxis, np.newaxis] * delta_ntar_mcmc)
+
+    beta_tar_lower = np.quantile(beta_tar_mcmc, q=0.025, axis=0)
+    beta_tar_upper = np.quantile(beta_tar_mcmc, q=0.975, axis=0)
+    beta_ntar_lower = np.quantile(beta_ntar_mcmc, q=0.025, axis=0)
+    beta_ntar_upper = np.quantile(beta_ntar_mcmc, q=0.975, axis=0)
+
+    # posterior mode over mcmc iterations
+    LDAGibbsObj.save_lda_selection_indicator(
+        delta_tar_mcmc_mean, delta_ntar_mcmc_mean,
+        lambda_mcmc_mean, gamma_mcmc_mean,
+        message_eeg, gc.sub_file_name[:4], phi_fn, threshold=plot_threshold, mcmc=True,
+        beta_tar_lower=beta_tar_lower,
+        beta_tar_upper=beta_tar_upper,
+        beta_ntar_lower=beta_ntar_lower,
+        beta_ntar_upper=beta_ntar_upper
+    )
+
+    # Visualize log-likelihood over MCMC iterations (we don't know the truth for real data, so I use 0 instead.)
+    true_log_lhd = np.zeros([gc.num_electrode])
+    LDAGibbsObj.save_mcmc_trace_plot(
+        rho_mcmc, s_sq_mcmc, lambda_mcmc, log_lhd_mcmc, gamma_mcmc_mean,
+        true_log_lhd, gc.sub_file_name[:4]
+    )
+
+    # classification
+    lda_bayes_result = LDAGibbsObj.produce_lda_bayes_result_dict(
+        signals_all, eeg_code_3d,
+        delta_tar_mcmc, delta_ntar_mcmc,
+        lambda_mcmc,
+        gamma_mcmc, s_sq_mcmc, rho_mcmc,
+        phi_fn, trn_repetition, gc.target_letters,
+        channel_ids=np.array([5])
+    )
+
+    print('Proportion of correct prediction:')
+    mean_prop_pred = lda_bayes_result['mean']
+    max_prop_pred = lda_bayes_result['max']
+    letter_max_prop_pred = lda_bayes_result['letter_max']
+
+    for i, letter_i in enumerate(gc.target_letters):
+        print('{}, Train:'.format(letter_i))
+        print('Correctly pred: {}'.format(mean_prop_pred[i, :trn_repetition]))
+        print('Max prob: {}'.format(max_prop_pred[i, :trn_repetition]))
+        print('Max prob letter: {}'.format(letter_max_prop_pred[i, :trn_repetition]))
+        print('{}, Test:'.format(letter_i))
+        print('Correctly pred: {}'.format(mean_prop_pred[i, trn_repetition:]))
+        print('Max prob: {}'.format(max_prop_pred[i, trn_repetition:]))
+        print('Max prob letter: {}'.format(letter_max_prop_pred[i, trn_repetition:]))
+
+    LDAGibbsObj.save_lda_bayes_results(lda_bayes_result, message_eeg, gc.target_letters)
