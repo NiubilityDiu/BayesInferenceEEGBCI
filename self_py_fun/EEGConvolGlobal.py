070aa86c3b933b830066bca4b80a1faa61edd72c
NiubilityDiu <mtianwen@umich.edu>
2020-01-06 11:08:26 -0500

    Move hyper-parameters to global file
    better control the real data because we separate mcmc procedure and prediction procedure
    keep the result consistent.

--- self_py_fun/EEGConvolGlobal.py
+++ self_py_fun/EEGConvolGlobal.py
@@ -18,7 +18,7 @@ letter_dim = 19
 serial_num = '001'
 exper_name = 'BCI'  # BCI, CMP...
 data_type = 'TRN'  # Choose among TRN_files, FRT_files and NOC_files
-n_multiple = 5
+n_multiple = 4
 sub_file_name = 'K' + str(K_num) + '_' + \
                 serial_num + '_' + \
                 exper_name + '_' + data_type
@@ -59,14 +59,31 @@ n_length = n_multiple * flash_and_pause_length
 # eps_kernel_scale = 0.5*np.ones([num_electrode])
 
 # Gibbs sampling:
-kappa = 3200
+kappa = 2500
+u = 8
+alpha_s = 5.0
+beta_s = 5.0
+zeta_lambda = 1e-4 * np.ones([num_electrode])
+zeta_s = 5e-3 * np.ones([num_electrode])
+zeta_rho = 1e-4 * np.ones([num_electrode])
+ki = 0.4
+scale_1 = 0.15
+scale_2 = 0.2
+std_bool = False
+beta_ising = 0.1
+gamma_neighbor = 2
+plot_threshold = 0.5
+a = 1  # weight for target
+b = 5  # weight for non-target
+NUM_INTERVAL = 200
+
 # HMC Inference
-step_size_init = 1e-4
-num_steps_between_results = 1
-num_leapfrog_steps = 5
-n_samples = 100
-n_burn_in = 2000
-target_accept_prob = 0.5
+# step_size_init = 1e-4
+# num_steps_between_results = 1
+# num_leapfrog_steps = 5
+# n_samples = 100
+# n_burn_in = 2000
+# target_accept_prob = 0.5
 
 if method_name is 'convol_python':
     var_key = 'mcmc'
@@ -75,19 +92,16 @@ else:
 
 # total_seq_length = int((num_rep * num_repetition + 4) * flash_and_pause_length)
 # trn_total_seq_length = int((num_rep * trn_repetition + 4) * flash_and_pause_length)
-
-lambda_p = 10
-# WLS use
-lambda_s = 100
-lambda_0 = 100
-iter_num = 31
-jitter = 10
-PLOT_INTERVAL = 5
-DAT_TYPE = 'float32'
-NUM_INTERVAL = 200
-
-electrode_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], dtype=np.int32) - 1
-electrode_dim = len(electrode_ids)
+# lambda_p = 10
+# # WLS use
+# lambda_s = 100
+# lambda_0 = 100
+# iter_num = 31
+# jitter = 10
+# PLOT_INTERVAL = 5
+# DAT_TYPE = 'float32'
+# electrode_ids = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], dtype=np.int32) - 1
+# electrode_dim = len(electrode_ids)
 
 true_row_indices = [4, 2, 1, 6, 3, 4, 2, 1, 2, 6, 1, 3, 3, 4, 3, 6, 1, 3, 4]
 true_col_indices = [8, 8, 11, 12, 11, 9, 9, 9, 11, 12, 8, 12, 9, 11, 8, 12, 12, 9, 12]
