a356bc28253110a925d034bd4f68a9dbae0b8ad6
NiubilityDiu <mtianwen@umich.edu>
2022-03-04 17:39:00 -0800

    Delete script_slurm_system directory
    .pbs files are not necessary to include in the github repository.

--- script_slurm_system/Real_data_predict.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 5); do
-  for j in $(seq 0 15); do
-      export electrode_id_index=$j,mcmc_id=$i,mcmc_check_num=0,truncate_index=400
-      sbatch ./script_slurm_system/Real_data_predict.pbs
-  done
-done
--- script_slurm_system/sim_multi_rep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 24); do
-    export sim_name_id=$i
-    sbatch ./script_slurm_system/sim_multi_rep_direct.pbs
-    sbatch ./script_slurm_system/sim_multi_rep_convol.pbs
-done
